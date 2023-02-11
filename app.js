0b86287f3ed36839ec20b8559612591a8275c881
shinsangeun <s0813se@naver.com>
2023-02-12 00:37:43 +0900

    강의 1.3) moveTo, lineTo 연습

--- app.js
+++ app.js
@@ -4,12 +4,9 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.rect(50, 50, 100, 200);
-ctx.rect(150, 150, 100, 200);
-ctx.fill()
-
-ctx.beginPath();
-ctx.rect(250, 250, 100, 200);
-ctx.rect(450, 450, 100, 200);
-ctx.fillStyle = "blue";
-ctx.fill()
+ctx.moveTo(50, 50);
+ctx.lineTo(150, 50);
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
+ctx.fill();
