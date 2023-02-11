bad9686593dab1acdc16db07e9ac2a5b3eecda27
shinsangeun <s0813se@naver.com>
2023-02-12 00:53:43 +0900

    강의 1.6) 사람 만들기

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
+ctx.fillRect(190, 200, 15, 100);
+ctx.fillRect(330, 200, 15, 100);
+ctx.fillRect(240, 200, 60, 200);
 
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+ctx.arc(270, 140, 50, 0, 2*Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "white"
+ctx.arc(255, 130, 7, Math.PI, 2*Math.PI);
+ctx.arc(285, 130, 7, Math.PI, 2*Math.PI);
 ctx.fill();
