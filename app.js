e129c8e1d704c9788411b0abd53ab608e3e580d4
shinsangeun <s0813se@naver.com>
2023-02-12 01:10:01 +0900

    강의 2.1) 마우스 이벤트에 따라 선 그리기

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = 2;
 
-ctx.fillRect(190, 200, 15, 100);
-ctx.fillRect(330, 200, 15, 100);
-ctx.fillRect(240, 200, 60, 200);
+const colors = [
+    "red",
+    "blue",
+    "orange",
+    "yellow",
+    "purple"
+]
 
-ctx.arc(270, 140, 50, 0, 2*Math.PI);
-ctx.fill();
+// ctx.moveTo(0, 0);
 
-ctx.beginPath();
-ctx.fillStyle = "white"
-ctx.arc(255, 130, 7, Math.PI, 2*Math.PI);
-ctx.arc(285, 130, 7, Math.PI, 2*Math.PI);
-ctx.fill();
+function onclick(event){
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    ctx.strokeStyle = color;
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+}
+
+canvas.addEventListener("mousemove", onclick)
