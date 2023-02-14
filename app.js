27c6d4db4f426f9c8723325b2c6c0ad9c48d6fb3
shinsangeun <s0813se@naver.com>
2023-02-14 23:59:19 +0900

    강의 2.2) 라인 굵기 제어 추가

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const lineWidth = document.getElementById("line-width");
 
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 
 let isPainting = false;
 
 function onMove(event){
     if(isPainting){
         ctx.lineTo(event.offsetX, event.offsetY);
-        ctx.fill();
+        ctx.stroke();
         return;
     }
+    ctx.beginPath();
     ctx.moveTo(event.offsetX, event.offsetY);
 }
 
@@ -24,7 +26,13 @@ function cancelPainting(){
     isPainting = false;
 }
 
+function onLineWidthChange(event){
+    ctx.lineWidth = event.target.value;
+}
+
 canvas.addEventListener("mousemove", onMove)
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting)
 canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange)
